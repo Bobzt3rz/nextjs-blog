adcc7306240c9d6343020636726f59e10e6cdd24
Bobzt3rz <brunswick.99999@gmail.com>
2022-04-12 14:38:37 +0700

    Updated intro

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ allPostsData }) {
       </Head>
 
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hi, my name is Bob and this is my blog!</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
